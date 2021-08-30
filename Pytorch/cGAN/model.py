33c5bc4ee8e2f816135a6415998e8536a21f2dbe
KungHao <james19961119@gmail.com>
2021-08-30 12:44:00 +0800

    train-well cGAN

--- Pytorch/cGAN/model.py
+++ Pytorch/cGAN/model.py
@@ -13,12 +13,33 @@ class Generator(nn.Module):
             nn.Linear(512, 784)
         )
         
+        self.layer = nn.Sequential(
+            nn.Linear(100+10, 1)
+        )
+
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(128, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 1, 3, 1, 1)
+        )
+        
         self.label_emb = nn.Embedding(10, 10)
 
     def forward(self, z, label):
         label = self.label_emb(label)
         y = torch.cat((z, label), 1)
-        return self.model(y)
+        input = self.layer(y)
+        input = input.view(-1, 1, 1, 1)
+        return self.model(input)
 
 class Discriminator(nn.Module):
     def __init__(self):
@@ -32,10 +53,29 @@ class Discriminator(nn.Module):
             nn.Linear(256, 1),
             nn.Sigmoid()
         )
+        self.layer = nn.Sequential(
+            nn.Linear(784+10, 1)
+        )
+        
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Linear(128, 4096),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Linear(128, 1),
+            nn.Sigmoid()
+        )
         
         self.label_emb = nn.Embedding(10, 10)
 
     def forward(self, y, label):
         label = self.label_emb(label)
         x = torch.cat([y, label], 1)
-        return self.model(x)
+        input = self.layer(x)
+        input = input.view(-1, 1, 32, 32)
+        return self.model(input)
